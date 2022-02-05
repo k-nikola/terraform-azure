590bacffacdce0d04f01e8e420e8e81aa5758e92
k-nikola <nikola.kurbalija@hotmail.com>
2022-02-05 20:42:12 +0100

    Change output.

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "resource_group_id" {
   value = azurerm_resource_group.rg.id
 }
 output "public_ip" {
-  value = azurerm_container_group.ncg.public_ip
+  value = azurerm_container_group.ncg.ip_address
 }
